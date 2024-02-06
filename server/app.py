502a817f7c6a76c322b95cd23eb7db69861ee7cd
Uel-5 <kariukiwaweru8156@gmail.com>
2024-02-06 18:58:31 +0300

    app.py starter

---
+++ server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask, make_response, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+#from model import 
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Configure the database URI for SQLAlchemy
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pizzeria.db"
+
+# Disable modification tracking for SQLAlchemy (not recommended in production)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+#Initialize Flask-Migrate
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+################################################################
+
+# Run the Flask app
+
+if __name__ == "__main__":
+    app.run(port=5555, debug = True)
