6ba40f75ba8b3559d867a54b7589247c606c3f90
Uel-5 <kariukiwaweru8156@gmail.com>
2024-02-14 19:31:19 +0300

    added flask alembic

--- server/model.py
+++ server/model.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+from sqlalchemy import Sequence
 
 # Initialize SQLAlchemy
 
@@ -12,21 +13,32 @@ class Pizza(db.Model):
     piz_name = db.Column(db.String(255), nullable=False, unique=True)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    respizza = db.relationship("Restaurant_Pizza",back_populates="res_pizza", lazy=True)
      
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurants'
 
-    res_id = db.Column(db.Integer, Sequence(res_id_seq, primary_key=True))
+    res_id = db.Column(db.Integer, Sequence("res_id_seq"), primary_key=True)
     res_name = db.Column(db.String(255), unique=True) 
     res_address = db.Column(db.String(255), nullable=False)
+
+    resResto = db.relationship("Restaurant_Pizza", back_populates=" res_restaurant")
 
 
 
 class Restaurant_Pizza(db.Model):
     __tablename__ = "respizza"
 
-    respizza_id = db.Column(db.Integer, Sequence("respizza_id_seq", primary_key=True))    
+    respizza_id = db.Column(db.Integer, Sequence("respizza_id_seq"), primary_key=True)    
     respizza_price = db.Column(db.Integer)
-    created_at = db.Column(db.DateTime, server_default= func.now())
+    pizzas_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
+    created_at = db.Column(db.DateTime, server_default= db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    res_pizza = db.relationship("Pizza", back_populates="respizza", lazy=True)
+
+    res_restaurant= db.relationship("Restaurant", back_populates= "resResto", lazy=True)
+
